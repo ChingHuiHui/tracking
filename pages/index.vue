2b51e4d8f0bcaab478416a42a838e6d8a47c86eb
chinghuihui <sunny70624@gmail.com>
2023-02-27 15:27:42 +0800

    set up the liff

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="container mx-auto">
-    <h1 class="text-5xl font-bold">Tracking</h1>
+  <div
+    class="h-screen container mx-auto flex flex-col justify-center items-center"
+  >
+    <h1 class="text-5xl font-bold mb-4">Tracking</h1>
+    <ul>
+      <li>
+        Version：<span class="font-bold">{{ sdkVersion }}</span>
+      </li>
+      <li>
+        Error：<span :class="{ 'text-red-500': liffError }">{{
+          liffError || 'no any error'
+        }}</span>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+const { $liffInit, $liff } = useNuxtApp()
+
+const sdkVersion = ref('')
+const liffError = ref('')
+
+onMounted(() => {
+  $liffInit
+    .then(() => {
+      sdkVersion.value = $liff.getVersion()
+    })
+    .catch((error) => {
+      liffError.value = error
+    })
+})
+</script>
---
+++ plugins/liff.client.ts
@@ -0,0 +1,25 @@
+import liff from '@line/liff'
+
+export default defineNuxtPlugin(() => {
+  const liffInit = liff
+    .init({ liffId: useRuntimeConfig().public.liffId })
+    .then(() => {
+      console.log('liff.init() done')
+    })
+    .catch((error) => {
+      console.log(`liff.init() failed: ${error}`)
+      if (!process.env.liffId) {
+        console.info(
+          'LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable.'
+        )
+      }
+      return Promise.reject(error)
+    })
+
+  return {
+    provide: {
+      liff,
+      liffInit,
+    },
+  }
+})
